d17a639c4e235eada0a53ab4213bb5fffc6e076c
tangZhenTian0626 <112739856+tangZhenTian0626@users.noreply.github.com>
2022-09-05 19:07:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_access_token():
     try:
         access_token = get(post_url).json()['access_token']
     except KeyError:
-        print("获取access_token失败，请检查app_id和app_secret是否正确")
+        print("获取access_token失败，请检查app_id和app_secret是否正确"+access_token)
         os.system("pause")
         sys.exit(1)
     # print(access_token)
